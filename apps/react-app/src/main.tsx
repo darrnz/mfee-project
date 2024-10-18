658e1c27a289276a5b824af2e638795d3e7c4ccc
Diego Arroniz <63981028+darrnz@users.noreply.github.com>
2024-10-18 19:59:47 +0000

    graphql apollo configuration

--- apps/react-app/src/main.tsx
+++ apps/react-app/src/main.tsx
@@ -2,11 +2,15 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
 import App from './App';
+import { ApolloProvider } from '@apollo/client';
+import client from './apollo-client';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <StrictMode>
+    <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>
   </StrictMode>
 );
